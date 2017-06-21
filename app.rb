aa046a83cdde8774ecc6c0900fb75b157c7581e8
FreshOceans <mperezz398@gmail.com>
2017-06-21 14:16:48 -0400

    began database method

--- app.rb
+++ app.rb
@@ -2,9 +2,21 @@
 require "sinatra"
 require 'sinatra/activerecord'
 require "sinatra/reloader" if development?
+require "sass"
 
 # ======= database =======
 # set :database, "sqlite3:portfolio.db"  # sets the db name (will be created if it does not exist)
+
+# ===== five users =====
+def make_user_data
+	@@user_array: []
+	text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	formatted_text = text.split
+	puts formatted_text
+
+end
+make_user_data
+
 
 # ======= home =======
 get '/' do
@@ -16,13 +28,13 @@ end
 # 	puts "\n******* home *******"
 # 	erb :home
 # end
-# ======= signup form =======
-get '/signup_form' do
-	puts "\n******* signup *******"
-	erb :signup_form
+# ======= register form =======
+get '/register_form' do
+	puts "\n******* register *******"
+	erb :register_form
 end
-# ====== signup ======
-post '/signup' do
+# ====== register ======
+post '/register' do
     puts "params: #{params.inspect}"
     @username = params[:username]
     @password = params[:password]
